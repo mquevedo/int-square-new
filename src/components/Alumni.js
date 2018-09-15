044507d441c5f10483ef1fb1354b676ea6094330
William Martinez <williamthemiz@gmail.com>
2018-09-15 14:17:58 -0600

    bootstrap grid demo

--- src/components/Alumni.js
+++ src/components/Alumni.js
@@ -1,4 +1,13 @@
-// aqui ira un slider con info de los int2
 import React from 'react'
 
-export const Alumni = () => <div>Alumni</div>
+export const Alumni = () => {
+  return (
+    <div className="container-fluid" style={{background:'blue'}}>
+      <div className="row" style={{background:'skyblue'}}>
+        <div className="col-sm-12 col-md-6 col-lg-4" style={{border:'5px solid blue'}}>parte 1</div>
+        <div className="col-sm-12 col-md-6 col-lg-4" style={{border:'5px solid blue'}}>parte 2</div>
+        <div className="col-sm-12 col-md-6 col-lg-4" style={{border:'5px solid blue'}}>parte 3</div>
+      </div>
+    </div>
+  )
+}

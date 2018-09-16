0ad5d04cb8433e7fc4b2144ed22c9a4440508ecd
Josue García <garciajosue402@gmail.com>
2018-09-15 21:00:43 -0600

    Fixed padding and center components

--- src/components/Partners.js
+++ src/components/Partners.js
@@ -8,24 +8,22 @@ export const Partners = () => {
   return (
     <PartnersSectionStyled>
       <OuterStyledSection className='partners__outer'>
-        <InnerStyledSection className='partners__inner'>
-          <div className='partners__data'>
-            <p> OUR PARTNERS </p>
-            <div className='partners__data__gen-ui'>
-              <div className='partners__data__gen-ui--layer'>
-                <div className='partners__data__gen-ui--layer--logo'>
-                  <p> SEATTLE, WA </p></div>
-              </div>
+        <div className='partners__data'>
+          <p> OUR PARTNERS </p>
+          <div className='partners__data__gen-ui'>
+            <div className='partners__data__gen-ui--layer'>
+              <div className='partners__data__gen-ui--layer--logo'>
+                <p> SEATTLE, WA </p></div>
             </div>
-            <div className='partners__data__ibes'>
-              <div className='partners__data__ibes--layer'>
-                <div className='partners__data__ibes--layer--logo'>
-                  <p> GERMANY </p>
-                </div>
+          </div>
+          <div className='partners__data__ibes'>
+            <div className='partners__data__ibes--layer'>
+              <div className='partners__data__ibes--layer--logo'>
+                <p> GERMANY </p>
               </div>
             </div>
           </div>
-        </InnerStyledSection>
+        </div>
       </OuterStyledSection>
     </PartnersSectionStyled>
   )
@@ -41,28 +39,9 @@ const PartnersSectionStyled = styled.div`
             background-color: #E4E4E4;  
         }
 
-        &__inner {
-            height: inherit;
-            padding: 0;
-            font-size: 16px;
-            letter-spacing: 1.2px;
-            display: flex;
-            flex-flow: column;
-            flex-direction: row;
-            align-items: flex-start;
-            @media (min-width: ${({theme}) => theme.medium.start}) {
-                width: 90%;
-                font-size: 20px;
-                letter-spacing: 1.5px;
-            }
-            @media (min-width: ${({ theme }) => theme.large.start}) {
-               width: 100%;
-               justify-content: none !important;
-            } 
-        }
-
         &__data {
             justify-content: space-evenly;
+            
             p {
                 margin-right: 70px;
                 color: ${({theme}) => theme.blue};
@@ -72,8 +51,10 @@ const PartnersSectionStyled = styled.div`
 
              @media (min-width: ${({ theme }) => theme.medium.start}) {
                 width: 100%;
+                padding-left: 40px;
+                padding-right: 40px;
                 p { 
-                    margin-right: 55%;
+                    text-align: left;
                     width: 100%;
                     font-size: 35px;
                 }
@@ -113,21 +94,23 @@ const PartnersSectionStyled = styled.div`
                         background-repeat: no-repeat;
                         background-position: center;      
                         width: 80%;
-                        height: 80%;
+                        height: 100%;
                         p {
                             font-family: ${({theme}) => theme.baseFont};
                             margin: 0;
-                            margin-top: 65%;
+                            margin-top: 80%;
                             color: white;
                         }
 
                         @media (min-width: ${({ theme }) => theme.medium.start}) {  
-                            margin-top: 95%;
+                            margin-top: 0% !important;
+                            margin-bottom: 20%;
                             width: 60%;
                             height: 60%;
                             p {
                                 width: 100%;
-                                margin-top: 150px;
+                                margin-left: 20%;
+                                margin-top: 200px;
                                 font-size: 25px;
                             }
                         } 
@@ -135,16 +118,18 @@ const PartnersSectionStyled = styled.div`
                         @media (min-width: ${({ theme }) => theme.medium.end}) {  
                             margin-top: 75%;
                             p {
-                                margin-top: 150 px;
+                                margin-top: 200px;
                             }
                         } 
 
                         @media (min-width: ${({ theme }) => theme.large.start}) {  
                             margin-top: 0%;
+                            margin-bottom: 0;
                             width: 60%;
-                            height: 45%;
+                            height: 100% !important;
                             p {
-                                margin-top: 50%;
+                                margin-top: 90%;
+                                margin-left: 10%;
                                 font-size: 30px;
                             }
                         } 
@@ -180,17 +165,18 @@ const PartnersSectionStyled = styled.div`
                         background-repeat: no-repeat;
                         background-position: center;      
                         width: 50%;
-                        height: 50%;
+                        height: 100%;
                         p {
                             font-family: ${({theme}) => theme.baseFont};
                             margin: 0;
-                            margin-top: 75%;
+                            margin-top: 115%;
                             color: white;
                         }
 
-                        @media (min-width: ${({ theme }) => theme.medium.start}) { 
+                        @media (min-width: ${({ theme }) => theme.medium.start}) {
+                            margin-bottom: 38%;
                             width: 30%;
-                            height: 35%;
+                            height: 40%;
                             p {
                                 margin-top: 146px;
                                 font-size: 28px;
@@ -205,10 +191,11 @@ const PartnersSectionStyled = styled.div`
                         } 
 
                         @media (min-width: ${({ theme }) => theme.large.start}) {
+                            margin-bottom: 0%;
                             width: 40%;
-                            height: 40%;   
+                            height: 100%;   
                             p {
-                                margin-top: 70%;
+                                margin-top: 125%;
                                 font-size: 45px;
                             }
                         } 

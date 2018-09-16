7325c0ea963f2b5cdcf215f7ac0f16d5863c0a1d
Josue García <garciajosue402@gmail.com>
2018-09-15 19:03:01 -0600

    Import brands

---
+++ src/components/Brands/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BrandsSection }  from './BrandsSection'
+import { CompaniesSection } from './CompaniesSection'
+
+export const Brands = () => {
+  return (
+    <div>
+      <CompaniesSection />
+      <BrandsSection />
+    </div>
+  )
+}

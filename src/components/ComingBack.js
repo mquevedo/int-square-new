7348eef227603672a79b3ae1780bdb57475895fa
William Martinez <williamthemiz@gmail.com>
2018-09-19 21:09:26 -0600

    update slider and comeback with real info

--- src/components/ComingBack.js
+++ src/components/ComingBack.js
@@ -7,30 +7,34 @@ export const ComingBack = () => {
       <div className="coming-back">
         <div className="coming-back__header">
           <h1 className="coming-back__header__title">COMING BACK</h1>
-          <p className="coming-back__header__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          <p className="coming-back__header__text">
+            What kind of opportunities lie ahead after returning to El Salvador?
           </p>
         </div>
         <div className="coming-back__items row justify-content-center">
           <div className="coming-back__item col-sm-12 col-lg-4">
             <img className="coming-back__item__image" src="https://int2.blob.core.windows.net/public/svg/comingback-startup.svg" alt="startup"/>
             <h2 className="coming-back__item__title">Create your own start up</h2>
-            <p className="coming-back__item__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <p className="coming-back__item__text">
+              With the experience obtained by the participants and the exposure to new technologies 
+              and business models some of them have decided to start their own startup.
             </p>
           </div>
           <div className="coming-back__item col-sm-12 col-lg-4">
             <img className="coming-back__item__image coming-back__item__image--right" src="https://int2.blob.core.windows.net/public/svg/comingback-korinver.svg" alt="korinver"/>
             <h2 className="coming-back__item__title">Work with Korinver</h2>
-            <p className="coming-back__item__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <p className="coming-back__item__text">
+              Every participant returns with a contract for 2 years. 
+              Each one keeps working with their receiving company as a continuation of the training 
+              that was obtained abroad.
             </p>
           </div>
           <div className="coming-back__item col-sm-12 col-lg-4">
             <img className="coming-back__item__image" src="https://int2.blob.core.windows.net/public/svg/comingback-work.svg" alt="workremotely"/>
-            <h2 className="coming-back__item__title">Work Remotely</h2>
-            <p className="coming-back__item__text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
-              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <h2 className="coming-back__item__title">Work internationally </h2>
+            <p className="coming-back__item__text">
+              The establishment of long term alliances with our partners allow us to keep 
+              working with them from El Salvador after the contract period is finished.
             </p>
           </div>
         </div>

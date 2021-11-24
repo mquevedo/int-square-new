6a31cb1dc2a68d2b87504fdc6c49980a9c340a36
William Martinez <william.martinez@genui.com>
2021-11-23 21:11:59 -0600

    updating dependencies to latest versions

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render, screen } from '@testing-library/react'
 import App from './App'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
-  ReactDOM.unmountComponentAtNode(div)
+test('renders learn react link', () => {
+  render(<App />)
+  const linkElement = screen.getByText(/The Int2 project focuses on sending young professionals from El Salvador to companies abroad/i)
+  expect(linkElement).toBeInTheDocument()
 })
